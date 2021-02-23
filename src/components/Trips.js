96f5c1574059f83af7f15be6f35bedfc46d97578
Lario <osunkunleolanrewaju@gmail.com>
2021-02-23 22:30:25 +0100

    fetch and render trips data using graphql

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
+import {useStaticQuery, graphql} from 'gatsby'
+import Img from "gatsby-image"
 
 const Trips = () => {
+    const data = useStaticQuery(graphql`
+    query TripsQuery {
+        allTripsJson {
+          edges {
+            node {
+              alt
+              button
+              name
+              img {
+                childrenImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
+
+    function getTrips(data){
+        const tripsArray = []
+        data.allTripsJson.edges.forEach((item, index) => {
+            tripsArray.push(
+                <div key={index}>
+                    <Img 
+                    src={item.node.img.childrenImageSharp[0].fluid.src}
+                    fluid={item.node.img.childrenImageSharp[0].fluid}
+                    />
+                </div>
+            )
+        })
+
+        return tripsArray
+    }
+
     return (
         <ProductContainer>
             <ProductHeading>Heading</ProductHeading>
-            <ProductWrapper>Wrapper</ProductWrapper>
+            <ProductWrapper>{getTrips(data)}</ProductWrapper>
         </ProductContainer>
     )
 }
